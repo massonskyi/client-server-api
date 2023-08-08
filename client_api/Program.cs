de9f7278969b7d6acb2589f394a886d1e162c17d
Andrey <2621ag@gmail.com>
2023-08-08 16:06:35 +0300

    first_commit

---
+++ client_api/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string url = "http://127.0.0.1:8001";
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response received: " + responseBody);
+            }
+            else
+            {
+                Console.WriteLine("Request failed. Status code: " + response.StatusCode);
+            }
+        }
+    }
+}
+
+
+
